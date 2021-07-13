19cc0b0628799fcdf317cf4a038b6b7037cdebe4
Eliana995 <eliana.ag.95@gmail.com>
2021-07-13 15:36:11 -0300

    my first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const mainRouter = require('./routers/main');
+
+app.use('/',mainRouter);
+
+
+app.listen(port, () => console.log('servidor corriendo en el puerto ' + port));
